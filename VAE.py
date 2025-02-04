53d2f9aa48a99d43b59ce0c934dc4ee954b44b4c
ZiedMarr <zied.marrakchi2001@gmail.com>
2025-02-04 15:06:25 +0100

    fix vae forward

--- VAE.py
+++ VAE.py
@@ -57,7 +57,7 @@ class VAE(nn.Module):
         return self.decoder(z)
 
     def forward(self, x):
-        mu, log_var = self.encode(x)
+        mu, log_var = self.encode_full(x)
         z = self.reparameterize(mu, log_var)
         return self.decode(z), mu, log_var, z
 
