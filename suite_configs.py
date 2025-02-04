b43f7a269f3c993c1a9305e1d7c0831c4fd819c9
ZiedMarr <zied.marrakchi2001@gmail.com>
2025-02-04 11:31:10 +0100

    v10 configs

--- suite_configs.py
+++ suite_configs.py
@@ -2,7 +2,7 @@ SUITE_CONFIGS = {
     'config_A': {
     'INPUT_STATE_SIZE' : 3,
     'OUTPUT_STATE_SIZE' : 3,
-    'LATENT_DIM' : 4,
+    'LATENT_DIM' : 3,
 
 
 
@@ -13,17 +13,17 @@ SUITE_CONFIGS = {
     'DECODER_HIDDEN2': 10,
     'DECODER_HIDDEN3' : 12,
 
-    'BETA_KL_DIV': 0.13,
+    'BETA_KL_DIV': 0.12,
 
     'EVAL_SEED' : [1, 33, 545, 65 ,6 , 66, 78, 48 , 24 , 98],
 
-    'VAE_Version' : "VAE_Version_9"
+    'VAE_Version' : "VAE_Version_10"
 
     },
     'config_B': {
     'INPUT_STATE_SIZE' : 3,
     'OUTPUT_STATE_SIZE' : 3,
-    'LATENT_DIM' : 4,
+    'LATENT_DIM' : 3,
 
 
 
@@ -38,7 +38,47 @@ SUITE_CONFIGS = {
 
     'EVAL_SEED' : [1, 33, 545, 65 ,6 , 66, 78, 48 , 24 , 98],
 
-    'VAE_Version' : "VAE_Version_9"
+    'VAE_Version' : "VAE_Version_10"
+    },
+'config_C': {
+    'INPUT_STATE_SIZE' : 3,
+    'OUTPUT_STATE_SIZE' : 3,
+    'LATENT_DIM' : 3,
+
+
+
+    'ENCODER_HIDDEN' : 12,
+    'ENCODER_HIDDEN2': 10,
+    'ENCODER_HIDDEN3' : 8,
+    'DECODER_HIDDEN' : 8,
+    'DECODER_HIDDEN2': 10,
+    'DECODER_HIDDEN3' : 12,
+
+    'BETA_KL_DIV': 0.13,
+
+    'EVAL_SEED' : [1, 33, 545, 65 ,6 , 66, 78, 48 , 24 , 98],
+
+    'VAE_Version' : "VAE_Version_10"
+    },
+'config_D': {
+    'INPUT_STATE_SIZE' : 3,
+    'OUTPUT_STATE_SIZE' : 3,
+    'LATENT_DIM' : 3,
+
+
+
+    'ENCODER_HIDDEN' : 12,
+    'ENCODER_HIDDEN2': 10,
+    'ENCODER_HIDDEN3' : 8,
+    'DECODER_HIDDEN' : 8,
+    'DECODER_HIDDEN2': 10,
+    'DECODER_HIDDEN3' : 12,
+
+    'BETA_KL_DIV': 0.1,
+
+    'EVAL_SEED' : [1, 33, 545, 65 ,6 , 66, 78, 48 , 24 , 98],
+
+    'VAE_Version' : "VAE_Version_10"
     },
     # Add additional configurations as needed
 
