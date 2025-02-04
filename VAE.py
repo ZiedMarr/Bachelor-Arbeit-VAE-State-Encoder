318fcbb5f4dcd54f98af4cf36f4d384d5572c4c3
ZiedMarr <zied.marrakchi2001@gmail.com>
2025-02-04 14:58:12 +0100

    changed vae to batch norm vae

--- VAE.py
+++ VAE.py
@@ -1,72 +1,60 @@
-import torch
 import torch.nn as nn
-
+import torch
 import config
-#TODO : Think about if it's better to separate the network that produces mu and sigma
-#TODO : Think if we should use the log of sigma or not
+
 
 class VAE(nn.Module):
     def __init__(self, input_dim, latent_dim, output_dim):
-        """
-        Variational Autoencoder (VAE) for predicting m next states from n input states.
-        :param input_dim: Total dimension of the n stacked input states.
-        :param latent_dim: Dimension of the latent space.
-        :param output_dim: Total dimension of the m stacked output states (next states).
-        """
         super(VAE, self).__init__()
 
-        # Encoder: Maps input to latent space (outputs mean and log-variance)
+        # Encoder with Batch Normalization
         self.encoder = nn.Sequential(
             nn.Linear(input_dim, config.ENCODER_HIDDEN),
+            nn.BatchNorm1d(config.ENCODER_HIDDEN),
             nn.LeakyReLU(0.1),
-            nn.Linear(config.ENCODER_HIDDEN,config.ENCODER_HIDDEN2),
+
+            nn.Linear(config.ENCODER_HIDDEN, config.ENCODER_HIDDEN2),
+            nn.BatchNorm1d(config.ENCODER_HIDDEN2),
             nn.LeakyReLU(0.1),
+
             nn.Linear(config.ENCODER_HIDDEN2, config.ENCODER_HIDDEN3),
+            nn.BatchNorm1d(config.ENCODER_HIDDEN3),
             nn.LeakyReLU(0.1),
-            nn.Linear(config.ENCODER_HIDDEN3, config.LATENT_DIM * 2)  # Outputs: mean and log-variance
         )
 
-        # Decoder: Maps latent space to predicted next states
+        # Latent space layers
+        self.fc_mu = nn.Linear(config.ENCODER_HIDDEN3, latent_dim)
+        self.fc_var = nn.Linear(config.ENCODER_HIDDEN3, latent_dim)
+
+        # Decoder with Batch Normalization
         self.decoder = nn.Sequential(
-            nn.Linear(config.LATENT_DIM, config.DECODER_HIDDEN),
+            nn.Linear(latent_dim, config.DECODER_HIDDEN),
+            nn.BatchNorm1d(config.DECODER_HIDDEN),
             nn.LeakyReLU(0.1),
+
             nn.Linear(config.DECODER_HIDDEN, config.DECODER_HIDDEN2),
+            nn.BatchNorm1d(config.DECODER_HIDDEN2),
             nn.LeakyReLU(0.1),
-            nn.Linear(config.DECODER_HIDDEN2, config.DECODER_HIDDEN3),
-            nn.LeakyReLU(0.1),
-            nn.Linear(config.DECODER_HIDDEN3, output_dim)  # Outputs: predicted m next states
+
+            nn.Linear(config.DECODER_HIDDEN2, output_dim)
         )
-
-    def forward(self, x):
-        """
-        Forward pass through the VAE.
-        :param x: Input tensor of shape (batch_size, input_dim).
-        :return: predicted_next_states, mean, log_variance, latent_vector
-        """
-        # Encode input into latent space
-        encoded = self.encoder(x)
-        mu, log_var = encoded.chunk(2, dim=-1)  # Split into mean and log-variance
-        std = torch.exp(0.5 * log_var)
-        z = mu + std * torch.randn_like(std)  # Reparameterization trick
-
-        # Decode latent vector into predicted next states
-        predicted_next_states = self.decoder(z)
-
-
-        return predicted_next_states, mu, log_var, z
 
     def encode(self, x):
-        """
-        Encodes the input into the latent space.
-        :param x: Input tensor of shape (batch_size, input_dim).
-        :return: Latent mean (mu) of shape (batch_size, latent_dim).
-        """
-        encoded = self.encoder(x)
-        mu, _ = encoded.chunk(2, dim=-1)
-        return mu
+        h = self.encoder(x)
+        return self.fc_mu(h), self.fc_var(h)
+
+    def reparameterize(self, mu, log_var):
+        std = torch.exp(0.5 * log_var)
+        eps = torch.randn_like(std)
+        return mu + eps * std
 
     def decode(self, z):
-        pass
+        return self.decoder(z)
+
+    def forward(self, x):
+        mu, log_var = self.encode(x)
+        z = self.reparameterize(mu, log_var)
+        return self.decode(z), mu, log_var, z
 
     def MSE_Loss(self, mu , log_var, predicted_next_states, target_tensor):
         reconstruction_loss = torch.nn.MSELoss()(predicted_next_states, target_tensor)
@@ -97,4 +85,3 @@ class VAE(nn.Module):
         loss = reconstruction_loss + config.BETA_KL_DIV * kl_loss
 
         return loss
-    #TODO : Add decode function to check smoothness of latent space
