326c9263b33dc663ca2c3b86c000f6dc7b6e6a5a
ZiedMarr <zied.marrakchi2001@gmail.com>
2025-02-04 15:16:22 +0100

    v11 configs

--- suite_configs.py
+++ suite_configs.py
@@ -13,7 +13,7 @@ SUITE_CONFIGS = {
     'DECODER_HIDDEN2': 10,
     'DECODER_HIDDEN3' : 12,
 
-    'BETA_KL_DIV': 0.12,
+    'BETA_KL_DIV': 0.01,
     'LOSS_FUNC' : "MSE_loss_feature_Standardization",
 
     'EVAL_SEED' : [1, 33, 545, 65 ,6 , 66, 78, 48 , 24 , 98],
@@ -35,7 +35,7 @@ SUITE_CONFIGS = {
     'DECODER_HIDDEN2': 10,
     'DECODER_HIDDEN3' : 12,
 
-    'BETA_KL_DIV': 0.15,
+    'BETA_KL_DIV': 0.015,
     'LOSS_FUNC' : "MSE_loss_feature_Standardization",
 
     'EVAL_SEED' : [1, 33, 545, 65 ,6 , 66, 78, 48 , 24 , 98],
@@ -56,35 +56,13 @@ SUITE_CONFIGS = {
     'DECODER_HIDDEN2': 10,
     'DECODER_HIDDEN3' : 12,
 
-    'BETA_KL_DIV': 0.13,
+    'BETA_KL_DIV': 0.02,
     'LOSS_FUNC' : "MSE_loss_feature_Standardization",
 
     'EVAL_SEED' : [1, 33, 545, 65 ,6 , 66, 78, 48 , 24 , 98],
 
     'VAE_Version' : "VAE_Version_10"
-    },
-'config_D': {
-    'INPUT_STATE_SIZE' : 3,
-    'OUTPUT_STATE_SIZE' : 3,
-    'LATENT_DIM' : 4,
-
-
-
-    'ENCODER_HIDDEN' : 12,
-    'ENCODER_HIDDEN2': 10,
-    'ENCODER_HIDDEN3' : 8,
-    'DECODER_HIDDEN' : 8,
-    'DECODER_HIDDEN2': 10,
-    'DECODER_HIDDEN3' : 12,
-
-    'BETA_KL_DIV': 0.1,
-    'LOSS_FUNC' : "MSE_loss_feature_Standardization",
-
-
-    'EVAL_SEED' : [1, 33, 545, 65 ,6 , 66, 78, 48 , 24 , 98],
-
-    'VAE_Version' : "VAE_Version_11"
-    },
+    }
     # Add additional configurations as needed
 
 }
