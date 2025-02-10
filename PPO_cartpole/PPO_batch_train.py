ca2946ad53dd1374256538758b3517a98eb8f7b3
ZiedMarr <zied.marrakchi2001@gmail.com>
2025-02-10 10:50:30 +0100

    added eval cofig saving to ppo train

--- PPO_cartpole/PPO_batch_train.py
+++ PPO_cartpole/PPO_batch_train.py
@@ -1,7 +1,7 @@
 import os
 from PPO_cartpole.train_ppo_cartpole import train_ppo_cartpole
-from configs.config import EVAL_SEED
-
+from configs import eval_config
+from configs.save_config import save_eval_config
 
 # Get the current script's directory
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -13,11 +13,13 @@ batch = "batch_20000_timesteps_rand_env"
 batch_size = 5
 
 #define number of total training time steps :
-total_timesteps = 50000
+total_timesteps = 100000
 
 log_batch_dir = os.path.join(script_dir,"logs", "explore_rand_env" , batch)
+#save eval configs :
+save_eval_config(log_batch_dir)
 
-seeds = EVAL_SEED
+seeds = eval_config.EVAL_SEED
 
 for i in range(batch_size):
     train_ppo_cartpole(log_batch_dir=log_batch_dir, total_timesteps=total_timesteps, seed=seeds[i])
