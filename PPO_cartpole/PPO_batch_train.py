1e6260d347f4c12029da1e42004290e718d9593a
ZiedMarr <zied.marrakchi2001@gmail.com>
2025-02-11 13:21:57 +0100

    eval configs 3

--- PPO_cartpole/PPO_batch_train.py
+++ PPO_cartpole/PPO_batch_train.py
@@ -10,7 +10,7 @@ from Wrappers.RandomStartCartPole import RandomStartCartPole
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
 #name of log directory
-batch = "batch_explore_extremes_obs0"
+batch = "batch_evalconfig3"
 
 #define batch size
 batch_size = eval_config.BATCH_SIZE
@@ -18,11 +18,11 @@ batch_size = eval_config.BATCH_SIZE
 #define number of total training time steps :
 total_timesteps = eval_config.TOTAL_TIMESTEPS
 
-log_batch_dir = os.path.join(script_dir,"logs", "explore" , batch)
+log_batch_dir = os.path.join(script_dir,"logs", "eval" , batch)
 #save eval configs :
 save_eval_config(log_batch_dir)
 
 seeds = eval_config.EVAL_SEED
 
 for i in range(batch_size):
-    train_ppo_cartpole(log_batch_dir=log_batch_dir, total_timesteps=total_timesteps, seed=seeds[i], env_wrapper=RandomStartCartPole)
+    train_ppo_cartpole(log_batch_dir=log_batch_dir, total_timesteps=total_timesteps, seed=seeds[i], env_wrapper=RandomStartCartPoleEval)
